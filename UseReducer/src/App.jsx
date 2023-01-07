3e086dc7b117dd192fbdc211435fbb47b06b2258
jacob-d242 <jacobmoracha@gmail.com>
2023-01-07 23:11:12 +0300

    Creating reducers

--- UseReducer/src/App.jsx
+++ UseReducer/src/App.jsx
@@ -1,10 +1,50 @@
-import { useState } from 'react'
+import React from 'react'
+import { useReducer } from 'react'
 
+
+function NameList() {
+  const [state, dispatch] = useReducer(
+    (state, action) => { 
+      switch (action.type) {
+        case "SET_NAME":
+          return { ...state, name: action.payload };
+        case "ADD_NAME":
+          return {
+            ...state,
+            names: [...state.names,state.name],
+            name: "",
+          }
+      }
+      },{
+        names: [],
+        name:""
+  })
+  return (
+    <div className="App">
+      <div>{
+        state.names.map((name, index) => {
+          <div key={index}>{name}</div>
+          
+        })       
+      }</div>
+      
+        <input
+          type="text"
+          value={state.name}
+          onChange ={(e) => dispatch({type: "SET_NAME",payload: e.target.value})}
+        />
+      <div>{ state.name}</div>
+        <button
+          onClick={() => dispatch({type:"ADD_NAME"})}
+        >Add Name</button>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-     <h1>Moracha</h1>
+    <div>
+      <NameList/>
     </div>
   )
 }
